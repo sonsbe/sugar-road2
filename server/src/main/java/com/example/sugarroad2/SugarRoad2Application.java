971d06e787f649d6a7ce490ba876b9a96fdfc858
sonsbe <sbj9873@naver.com>
2023-10-31 16:42:11 +0900

    server 파일 업로드

---
+++ server/src/main/java/com/example/sugarroad2/SugarRoad2Application.java
@@ -0,0 +1,13 @@
+package com.example.sugarroad2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarRoad2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugarRoad2Application.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/sugarroad2/SugarRoad2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sugarroad2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugarRoad2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
