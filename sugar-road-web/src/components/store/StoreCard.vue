38d1b5822815b3be59ca3959cf34770b2379c40e
Your Name <jin0201948@gmail.com>
2023-11-16 09:49:39 +0900

    Add Vue store list page

--- sugar-road-web/src/components/store/StoreCard.vue
+++ sugar-road-web/src/components/store/StoreCard.vue
@@ -1,16 +1,54 @@
 <template>
-  <div class="store_card">
-    스토어 카드
-    <img />
-    <!-- <p class="store-title">{{ store.storeName }}</p> -->
+  <div class="storeBox">
+    <a href="#">
+      <div class="storeBoxTop">
+        <h2>가게명: {{ storeData.storeName }}</h2>
+        <div><span>추천수</span>, <span>리뷰수</span></div>
+      </div>
+      <img class="storeImg" v-bind:src="storeData.storeImagePath" />
+      <div class="store-desc">가게 설명:{{ storeData.storeDesc }}</div>
+    </a>
   </div>
 </template>
 
 <script setup>
-// const store = defineProps({
-//   storeName: String,
-//   storeImg: String,
-// });
+import { defineProps } from "vue";
+const { storeData } = defineProps(["storeData"]);
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+a {
+  text-decoration: none;
+}
+.storeBox {
+  border: 2px solid lightgray;
+  padding: 13px 8px;
+  margin-bottom: 2vh;
+  border-radius: 10px;
+  transition: 0.5s ease;
+  cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.15) 2.95px 2.95px 3.6px;
+}
+.storeBox > a {
+  text-decoration: none;
+}
+.storeBox:hover {
+  box-shadow: 1px 1px #e1d5ed, 2px 2px #e1d5ed, 3px 3px #e1d5ed, 4px 4px #e1d5ed,
+    5px 5px #e1d5ed, 6px 6px #e1d5ed;
+}
+.storeBoxTop {
+  display: flex;
+  justify-content: space-between;
+  height: 4vh;
+}
+.storeBoxTop div > span,
+.store-desc {
+  font-size: 14px;
+}
+.storeImg {
+  width: 100%;
+  height: 25vh;
+  object-fit: cover;
+  margin-bottom: 5px;
+}
+</style>
--- sugar-road-web/src/pages/store/Store.vue
+++ sugar-road-web/src/pages/store/Store.vue
@@ -1,9 +1,30 @@
 <template>
   <div>Store page</div>
-  <StoreCard class="container"></StoreCard>
+  <StoreCard
+    class="container"
+    v-for="store in storeList.value"
+    :key="store.storeId"
+    :storeData="{ storeName: store.storeName, address: store.address, phoneNumber:store.phoneNumber, storeDesc:store.storeDesc , storeImagePath: store.storeImagePath}"
+  ></StoreCard>
 </template>
 <script setup>
+import axios from "axios";
 import StoreCard from "../../components/store/StoreCard.vue";
+import { onMounted, reactive } from "vue";
+let storeList = reactive([]);
+onMounted(async () => {
+  await getStoreList();
+  console.log(storeList.value);
+});
+async function getStoreList() {
+  try {
+    const response = await axios.get("http://localhost:1023/store");
+    storeList.value = response.data;
+    console.log(storeList.value);
+  } catch (err) {
+    console.log(err);
+  }
+}
 </script>
 <style>
 .container {
