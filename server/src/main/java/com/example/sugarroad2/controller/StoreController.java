849326b5c4c238a0d1d4e322777af6800676ff45
Your Name <jin0201948@gmail.com>
2023-11-27 20:05:48 +0900

    feat pull main and merge

--- server/src/main/java/com/example/sugarroad2/controller/StoreController.java
+++ server/src/main/java/com/example/sugarroad2/controller/StoreController.java
@@ -2,7 +2,6 @@ package com.example.sugarroad2.controller;
 
 import com.example.sugarroad2.config.auth.NowUserDetails;
 import com.example.sugarroad2.model.dto.request.MenuRequestDTO;
-import com.example.sugarroad2.model.dto.request.StoreCreateRequestDTO;
 import com.example.sugarroad2.model.dto.request.StoreRequestDTO;
 import com.example.sugarroad2.model.dto.response.MenuResponseDTO;
 import com.example.sugarroad2.model.dto.response.StoreResponseDTO;
@@ -21,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
